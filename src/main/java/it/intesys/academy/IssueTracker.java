1feae9aa31f10205d46a742d9b7754633e3741e8
Enrico Costanzi <enrico.costanzi@intesys.it>
2023-05-19 15:20:04 +0200

    lesson1 - add logback

--- src/main/java/it/intesys/academy/IssueTracker.java
+++ src/main/java/it/intesys/academy/IssueTracker.java
@@ -1,10 +1,14 @@
 package it.intesys.academy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class IssueTracker {
 
-    public static void main(String[] args) {
+    private static Logger log = LoggerFactory.getLogger(IssueTracker.class);
 
+    public static void main(String[] args) {
         String message = AppConfiguration.appProperties().getProperty("app.message");
-        System.out.println(message);
+        log.info("Message: {}", message);
     }
 }
