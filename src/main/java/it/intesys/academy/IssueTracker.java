5d98ca929c1201a00281b840fe0339af5a85289d
biondadave <biondanidavide@gmail.com>
2023-05-31 18:04:43 +0200

    aggiunto messageDTO e e controller

--- src/main/java/it/intesys/academy/IssueTracker.java
+++ src/main/java/it/intesys/academy/IssueTracker.java
@@ -1,9 +1,29 @@
 package it.intesys.academy;
 
+import io.javalin.Javalin;
+import it.intesys.academy.dto.MessageDTO;
+import org.slf4j.Logger;
+
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
 public class IssueTracker {
+
+    private static Logger log= LoggerFactory.getLogger(IssueTracker.class);
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Issue Tracker");
+        MessageDTO messageDTO = new MessageDTO(getMessage(), new Date());
+
+        Javalin.create()
+                .get("/codylab", ctx -> ctx.json(messageDTO))
+                .start(Integer.valueOf(AppConfig.appProperties().getProperty(("server.port"))));
+
+       // System.out.println(message);
+        log.info(getMessage());
+    }
+    public static String getMessage(){
+        return AppConfig.appProperties().getProperty("app.message");
     }
 }
