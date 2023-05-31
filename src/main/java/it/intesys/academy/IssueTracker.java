412dd0dd4f6c22c34b42cca7c30d00eeefc2f5f0
alessiam1909 <ale.menon.vr@gmail.com>
2023-05-31 17:14:24 +0200

    Added javalin dependencies with localhost and message

--- src/main/java/it/intesys/academy/IssueTracker.java
+++ src/main/java/it/intesys/academy/IssueTracker.java
@@ -1,5 +1,6 @@
 package it.intesys.academy;
 
+import io.javalin.Javalin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,7 +8,11 @@ public class IssueTracker {
     private static Logger log = LoggerFactory.getLogger(IssueTracker.class);
     public static void main(String[] args) {
 
-        log.info("Message: {}", getMessage());
+        var applicationPort = Integer.parseInt(AppConfig.appProperties().getProperty("app.port"));
+        log.info("Start application on port {}", applicationPort);
+        var app = Javalin.create()
+                .get("/codylab", ctx -> ctx.result(getMessage()))
+                .start(applicationPort);
     }
 
     public static String getMessage(){
