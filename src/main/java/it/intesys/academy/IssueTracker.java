03936a444bda545dfd001b66fc8202b13a403dc5
zeno_begnini <zeno.begnini@gmail.com>
2023-05-31 12:15:59 +0200

    brench creation

--- src/main/java/it/intesys/academy/IssueTracker.java
+++ src/main/java/it/intesys/academy/IssueTracker.java
@@ -4,6 +4,6 @@ public class IssueTracker {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Issue Tracker");
+        System.out.println("Issue Tracker");
     }
 }
