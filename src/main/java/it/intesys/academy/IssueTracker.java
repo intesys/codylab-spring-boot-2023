9589028a089c949213f67bc49fe601f573585e4a
alessiam1909 <ale.menon.vr@gmail.com>
2023-05-31 16:36:12 +0200

    Added test dependencies

--- src/main/java/it/intesys/academy/IssueTracker.java
+++ src/main/java/it/intesys/academy/IssueTracker.java
@@ -6,7 +6,11 @@ import org.slf4j.LoggerFactory;
 public class IssueTracker {
     private static Logger log = LoggerFactory.getLogger(IssueTracker.class);
     public static void main(String[] args) {
-        var message = AppConfig.appProperties().getProperty("app.message");
-        log.info("Message: {}", message);
+
+        log.info("Message: {}", getMessage());
+    }
+
+    public static String getMessage(){
+       return AppConfig.appProperties().getProperty("app.message");
     }
 }
