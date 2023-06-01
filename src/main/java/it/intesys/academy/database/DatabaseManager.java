df984c8a3354a2620b360555deb1efb675f468aa
MATTIA ZATTONI <mattizatto@gmail.com>
2023-06-01 15:34:03 +0200

    lesson2 - ProjectService

--- src/main/java/it/intesys/academy/database/DatabaseManager.java
+++ src/main/java/it/intesys/academy/database/DatabaseManager.java
@@ -8,26 +8,29 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseManager {
-    private static final Logger log = LoggerFactory.getLogger(DatabaseManager.class);
+    private static final Logger log = LoggerFactory.getLogger(DriverManager.class);
     public static final String USER = "sa";
     public static final String PASSWORD = "password";
 
     public static Connection getConnection(){
         Connection conn = null;
         try{
-            conn = DriverManager.getConnection("C:\\Users\\matti\\OneDrive\\Desktop\\Stage\\06.01.2023\\lesson2.mv.db", USER, PASSWORD);
+            conn = DriverManager.getConnection("jdbc:h2:C:\\Users\\matti\\OneDrive\\Desktop\\Stage\\06.01.2023\\lesson2", USER, PASSWORD);
 
         }
         catch (SQLException ex){
             log.error("Errore type: ",ex);
         }
-        finally {
-            try {
-                conn.close();
-            }catch (SQLException e) {
-                log.error("Error type:",e);
-            }
-        }
         return conn;
     }
+    public static void closeConnection(Connection conn){
+        try{
+            if(conn != null){
+                conn.close();
+            }
+        }catch (SQLException ex){
+            log.error("Error type: ",ex);
+        }
+    }
 }
+
---
+++ src/main/java/it/intesys/academy/service/ProjectService.java
@@ -0,0 +1,37 @@
+package it.intesys.academy.service;
+
+import it.intesys.academy.database.DatabaseManager;
+import it.intesys.academy.dto.MessageDTO;
+
+import java.sql.Connection;
+import java.util.Collection;
+import java.util.Date;
+
+public class ProjectService {
+    public static MessageDTO testConnection(){
+        MessageDTO message = new MessageDTO();
+        message.setTimestamp(new Date());
+
+        Connection conn = DatabaseManager.getConnection();
+
+        if(conn != null){
+            message.setText("Connection");
+            DatabaseManager.closeConnection(conn);
+        }else{
+            message.setText("Connection failure");
+        }
+
+        return message;
+    }
+
+    private static boolean existTable(String tableName){
+        Connection conn = DatabaseManager.getConnection();
+
+        try{
+            return conn.createStatement().execute("SELECT * FROM "+tableName);
+        }catch (Exception e){
+            DatabaseManager.closeConnection(conn);
+        }
+        return false;
+    }
+}
