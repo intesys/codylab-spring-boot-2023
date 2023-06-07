88a8579ed9bf7b4edb9ba84a33b0e892397b68c9
Enrico Costanzi <enrico.costanzi@intesys.it>
2023-06-07 15:45:05 +0200

    lezione4 - add spring boot!

--- src/main/java/it/intesys/academy/AppConfiguration.java
+++ src/main/java/it/intesys/academy/AppConfiguration.java
@@ -1,34 +1,13 @@
 package it.intesys.academy;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import it.intesys.academy.service.ProjectService;
-import it.intesys.academy.service.PropertyMessageService;
-import it.intesys.academy.service.SettingsService;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import javax.sql.DataSource;
 
 @Configuration
-@PropertySource("classpath:application.properties")
-@ComponentScan("it.intesys.academy")
 public class AppConfiguration {
-
-
-    @Bean
-    DataSource dataSource(Environment environment) {
-        HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl(environment.getProperty("database.url"));
-        hikariConfig.setUsername(environment.getProperty("database.user"));
-        hikariConfig.setPassword(environment.getProperty("database.password"));
-        hikariConfig.setDriverClassName("org.h2.Driver");
-        return new HikariDataSource(hikariConfig);
-    }
 
     @Bean
     NamedParameterJdbcTemplate namedJdbcTemplate(DataSource dataSource) {
--- src/main/java/it/intesys/academy/IssueTracker.java
+++ src/main/java/it/intesys/academy/IssueTracker.java
@@ -5,16 +5,18 @@ import it.intesys.academy.service.ProjectService;
 import it.intesys.academy.service.PropertyMessageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
 
+@SpringBootApplication
 public class IssueTracker {
 
     private static final Logger log = LoggerFactory.getLogger(IssueTracker.class);
 
     public static void main(String[] args) {
-        ApplicationContext springContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
+        ConfigurableApplicationContext springContext = SpringApplication.run(IssueTracker.class, args);
 
         Environment environment = springContext.getEnvironment();
         var applicationPort = environment.getProperty("app.port", Integer.class);
