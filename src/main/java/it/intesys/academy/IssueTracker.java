de7e364cd2c9fa7306c90b2d6c9cb8f124666e11
Enrico Costanzi <enrico.costanzi@intesys.it>
2023-06-07 15:50:25 +0200

    lezione4 - add spring boot controller

--- src/main/java/it/intesys/academy/IssueTracker.java
+++ src/main/java/it/intesys/academy/IssueTracker.java
@@ -1,14 +1,9 @@
 package it.intesys.academy;
 
-import io.javalin.Javalin;
-import it.intesys.academy.service.ProjectService;
-import it.intesys.academy.service.PropertyMessageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class IssueTracker {
@@ -16,22 +11,7 @@ public class IssueTracker {
     private static final Logger log = LoggerFactory.getLogger(IssueTracker.class);
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext springContext = SpringApplication.run(IssueTracker.class, args);
-
-        Environment environment = springContext.getEnvironment();
-        var applicationPort = environment.getProperty("app.port", Integer.class);
-        log.info("Start application on port {}", applicationPort);
-
-        PropertyMessageService messageService = springContext.getBean(PropertyMessageService.class);
-        ProjectService projectService = springContext.getBean(ProjectService.class);
-
-        Javalin.create()
-                .get("/", ctx -> ctx.json(messageService.getMessage()))
-                .get("/list-projects", ctx -> {
-                    String username = ctx.queryParam("username");
-                    ctx.json(projectService.readProjects(username));
-                })
-                .start(applicationPort);
+        SpringApplication.run(IssueTracker.class, args);
     }
 
 }
---
+++ src/main/java/it/intesys/academy/controller/AppController.java
@@ -0,0 +1,34 @@
+package it.intesys.academy.controller;
+
+
+import it.intesys.academy.dto.MessageDTO;
+import it.intesys.academy.dto.ProjectDTO;
+import it.intesys.academy.service.ProjectService;
+import it.intesys.academy.service.PropertyMessageService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AppController {
+
+    private final PropertyMessageService propertyMessageService;
+    private final ProjectService projectService;
+
+    public AppController(PropertyMessageService propertyMessageService, ProjectService projectService) {
+        this.propertyMessageService = propertyMessageService;
+        this.projectService = projectService;
+    }
+
+    @GetMapping("/")
+    public MessageDTO getMessage() {
+        return propertyMessageService.getMessage();
+    }
+
+    @GetMapping("/list-projects")
+    public List<ProjectDTO> getProjects(@RequestParam String userName) {
+        return projectService.readProjects(userName);
+    }
+}
