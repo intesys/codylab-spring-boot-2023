f93dfa976cd982a9896350de9774071cc9cea6fd
Enrico Oliosi <enrico.oliosi@intesys.it>
2023-06-09 11:52:11 +0200

    ex - add repo, service and controller for Issue

--- src/main/java/it/intesys/academy/controller/ProjectController.java
+++ src/main/java/it/intesys/academy/controller/ProjectController.java
@@ -1,35 +1,33 @@
 package it.intesys.academy.controller;
 
-import it.intesys.academy.dto.MessageDTO;
 import it.intesys.academy.dto.ProjectDTO;
 import it.intesys.academy.service.ProjectService;
-import it.intesys.academy.service.PropertyMessageService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-public class AppController {
+public class ProjectController {
 
-    private final PropertyMessageService propertyMessageService;
     private final ProjectService projectService;
 
-    public AppController(PropertyMessageService propertyMessageService, ProjectService projectService) {
-        this.propertyMessageService = propertyMessageService;
+    public ProjectController(ProjectService projectService) {
         this.projectService = projectService;
-    }
-
-    @GetMapping("/")
-    public MessageDTO getMessage() {
-        return propertyMessageService.getMessage();
     }
 
     @GetMapping("/list-projects")
     public List<ProjectDTO> getProjects(@RequestParam String userName) {
 
-        return projectService.readProjects(userName);
+        return projectService.readProjectsWithIssues(userName);
+    }
+
+    @GetMapping("/project/{projectId}")
+    public ProjectDTO getProject(@PathVariable int projectId) {
+
+        return projectService.readProjectWithIssue(projectId);
     }
 
 }
---
+++ src/main/java/it/intesys/academy/repository/IssueRepository.java
@@ -0,0 +1,33 @@
+package it.intesys.academy.repository;
+
+import it.intesys.academy.dto.IssueDTO;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class IssueRepository {
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public IssueRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<IssueDTO> readIssues(List<Integer> projectIds) {
+
+        List<IssueDTO> issues =
+            jdbcTemplate.query("SELECT id, name, description, author, projectId FROM Issue WHERE projectId in (:projectIds)",
+
+                               Map.of("projectIds", projectIds),
+
+                               BeanPropertyRowMapper.newInstance(IssueDTO.class));
+
+        return issues;
+    }
+
+}
