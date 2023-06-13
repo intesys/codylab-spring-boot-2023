3b2c87ab01b15c0d710fb9d7ace1b46968a73c3f
Enrico Oliosi <enrico.oliosi@intesys.it>
2023-06-13 15:38:59 +0200

    lesson5 - add a MVC Controller and mustache template

--- src/main/java/it/intesys/academy/controller/rest/IssueController.java
+++ src/main/java/it/intesys/academy/controller/rest/IssueController.java
@@ -1,14 +1,16 @@
-package it.intesys.academy.controller;
+package it.intesys.academy.controller.rest;
 
 import it.intesys.academy.dto.IssueDTO;
 import it.intesys.academy.service.IssueService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class IssueController {
 
     private final IssueService issueService;
