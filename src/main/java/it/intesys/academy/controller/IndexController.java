2c61bf6972941b2b457a65d6b761bb8410748153
Enrico Oliosi <enrico.oliosi@intesys.it>
2023-06-13 15:38:59 +0200

    lesson5 - add a MVC Controller and mustache template

---
+++ src/main/java/it/intesys/academy/controller/IndexController.java
@@ -0,0 +1,32 @@
+package it.intesys.academy.controller;
+
+import it.intesys.academy.service.ProjectService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/mvc")
+public class IndexController {
+
+
+    private final ProjectService projectService;
+
+    public IndexController(ProjectService projectService) {
+
+        this.projectService = projectService;
+    }
+
+    @GetMapping("/list-projects")
+    public String index(Model model, @RequestParam String userName) {
+
+        model.addAttribute("username", userName);
+        model.addAttribute("projects", projectService.readProjectsWithIssues(userName));
+
+        return "index";
+
+    }
+
+}
--- src/main/java/it/intesys/academy/controller/rest/ProjectController.java
+++ src/main/java/it/intesys/academy/controller/rest/ProjectController.java
@@ -1,15 +1,17 @@
-package it.intesys.academy.controller;
+package it.intesys.academy.controller.rest;
 
 import it.intesys.academy.dto.ProjectDTO;
 import it.intesys.academy.service.ProjectService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class ProjectController {
 
     private final ProjectService projectService;
