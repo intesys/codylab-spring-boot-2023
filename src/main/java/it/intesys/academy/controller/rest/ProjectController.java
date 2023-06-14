dbca6b6781dcd7e1e4909b4472d2bca1524f7238
Enrico Costanzi <enrico.costanzi@intesys.it>
2023-06-14 18:32:03 +0200

    lesson6 - use ResponseEntity for http controllers

--- src/main/java/it/intesys/academy/controller/rest/ProjectController.java
+++ src/main/java/it/intesys/academy/controller/rest/ProjectController.java
@@ -2,6 +2,9 @@ package it.intesys.academy.controller.rest;
 
 import it.intesys.academy.dto.ProjectDTO;
 import it.intesys.academy.service.ProjectService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,6 +13,8 @@ import java.util.List;
 @RequestMapping("/api")
 public class ProjectController {
 
+    private final static Logger log = LoggerFactory.getLogger(ProjectController.class);
+
     private final ProjectService projectService;
 
     public ProjectController(ProjectService projectService) {
@@ -17,34 +22,39 @@ public class ProjectController {
     }
 
     @GetMapping("/projects")
-    public List<ProjectDTO> getProjects(@RequestParam String userName) {
+    public ResponseEntity<List<ProjectDTO>> getProjects(@RequestParam String userName) {
 
-        return projectService.readProjectsWithIssues(userName);
+        return ResponseEntity.ok(projectService.readProjectsWithIssues(userName));
     }
 
     @GetMapping("/projects/{projectId}")
-    public ProjectDTO getProject(@PathVariable int projectId, @RequestParam String username) {
+    public ResponseEntity<ProjectDTO> getProject(@PathVariable int projectId, @RequestParam String username) {
 
-        return projectService.readProjectWithIssue(projectId, username);
+        return ResponseEntity.ok(projectService.readProjectWithIssue(projectId, username));
     }
 
     @PostMapping("/projects")
-    public ProjectDTO createProject(@RequestBody ProjectDTO projectDTO, @RequestParam String username) {
+    public ResponseEntity<ProjectDTO> createProject(@RequestBody ProjectDTO projectDTO, @RequestParam String username) {
         if (projectDTO.getId() != null) {
-            throw new RuntimeException("Bad request, id must be null when creating a new project");
+            log.error("Bad request, id must be null when creating a new project");
+            return ResponseEntity.badRequest().build();
+
         }
-        return projectService.createProject(projectDTO, username);
+        return ResponseEntity.ok(projectService.createProject(projectDTO, username));
     }
 
     @PutMapping("/projects/{projectId}")
-    public ProjectDTO updateProject(@PathVariable int projectId, @RequestBody ProjectDTO projectDTO, @RequestParam String username) {
+    public ResponseEntity<ProjectDTO> updateProject(@PathVariable int projectId, @RequestBody ProjectDTO projectDTO, @RequestParam String username) {
         if (projectDTO.getId() == null) {
-            throw new RuntimeException("Bad request, id must not be null when updating a project");
+            log.error("Bad request, id must not be null when updating a project");
+            return ResponseEntity.badRequest().build();
         }
         if (projectDTO.getId() != projectId) {
-            throw new RuntimeException("Bad request, id in path and in body must be the same");
+            log.error("Bad request, id in path and in body must be the same");
+            return ResponseEntity.badRequest().build();
         }
-        return projectService.updateProject(projectDTO, username);
+
+        return ResponseEntity.ok(projectService.updateProject(projectDTO, username));
     }
 
 }
