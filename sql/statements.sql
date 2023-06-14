11fb9d0bd4e00d38390f3d1c5b9d6627253dcd9f
Enrico Costanzi <enrico.costanzi@intesys.it>
2023-06-14 17:18:16 +0200

    lesson6 - add auto increment option to SQL tables

--- sql/statements.sql
+++ sql/statements.sql
@@ -1,17 +1,17 @@
 CREATE TABLE Project (
-                         id int not null,
+                         id int not null auto_increment,
                          name varchar(64) not null,
                          description varchar(255),
                          PRIMARY KEY (id)
 );
 
-INSERT INTO Project VALUES (1, 'First Project', 'This is a description of a the 1st project');
-INSERT INTO Project VALUES (2, 'Second Project', 'Description of a the 2nd project');
+INSERT INTO Project (name, description) VALUES ('First Project', 'This is a description of a the 1st project');
+INSERT INTO Project (name, description) VALUES ('Second Project', 'Description of a the 2nd project');
 
 -----------------------------------------------------------------------
 
 CREATE TABLE Issue (
-                       id int not null,
+                       id int not null auto_increment,
                        name varchar(128) not null,
                        description varchar(255),
                        author varchar(128) not null,
@@ -20,25 +20,25 @@ CREATE TABLE Issue (
                        FOREIGN KEY (projectId) REFERENCES Project(id)
 );
 
-INSERT INTO Issue VALUES (1, 'Form validation problem', 'Email is not validated', 'Enrico Oliosi', 1);
+INSERT INTO Issue (name, description, author, projectId) VALUES ('Form validation problem', 'Email is not validated', 'Enrico Oliosi', 1);
 
 
 
 -----------------------------------------------------------------------
 
 CREATE TABLE UserProject (
-                             id int not null,
+                             id int not null auto_increment,
                              username varchar(128) not null,
                              projectId int not null,
                              PRIMARY KEY (id),
                              FOREIGN KEY (projectId) REFERENCES Project(id)
 );
 
-INSERT INTO UserProject VALUES (1, 'eoliosi', 1);
-INSERT INTO UserProject VALUES (2, 'ecostanzi', 2);
+INSERT INTO UserProject (username, projectId) VALUES ('eoliosi', 1);
+INSERT INTO UserProject (username, projectId) VALUES ('ecostanzi', 2);
 
 CREATE TABLE Comment (
-                         id int not null,
+                         id int not null auto_increment,
                          text varchar(2048),
                          author varchar(128) not null,
                          issueId int not null,
@@ -47,5 +47,5 @@ CREATE TABLE Comment (
 );
 
 
-insert into Comment values (1, 'This is a comment', 'ecostanzi', 1);
-insert into Comment values (2, 'This is another comment', 'ecostanzi', 1);
+insert into Comment (text, author, issueId) VALUES ('This is a comment', 'ecostanzi', 1);
+insert into Comment (text, author, issueId) VALUES ('This is another comment', 'ecostanzi', 1);
