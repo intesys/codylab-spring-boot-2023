d349c0899722c90c1115a0ec4df4e47f329cfc42
Enrico Costanzi <enrico.costanzi@intesys.it>
2023-06-15 11:03:05 +0200

    lesson6 - add delete mapping

--- src/main/java/it/intesys/academy/controller/rest/ProjectController.java
+++ src/main/java/it/intesys/academy/controller/rest/ProjectController.java
@@ -2,6 +2,10 @@ package it.intesys.academy.controller.rest;
 
 import it.intesys.academy.dto.ProjectDTO;
 import it.intesys.academy.service.ProjectService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +15,7 @@ import java.util.List;
 @RequestMapping("/api")
 public class ProjectController {
 
+    private Logger log = LoggerFactory.getLogger(ProjectController.class);
     private final ProjectService projectService;
 
     public ProjectController(ProjectService projectService) {
@@ -24,48 +29,57 @@ public class ProjectController {
     }
 
     @GetMapping("/projects/{projectId}")
-    public ProjectDTO getProject(@PathVariable int projectId, @RequestHeader("X-User-Name") String username) {
+    public ResponseEntity<ProjectDTO> getProject(@PathVariable int projectId, @RequestHeader("X-User-Name") String username) {
 
-        return projectService.readProjectWithIssue(projectId, username);
+        return ResponseEntity.ok(projectService.readProjectWithIssue(projectId, username));
     }
 
     @PostMapping("/projects")
-    public ProjectDTO createProject(@RequestBody ProjectDTO projectDTO,
+    public ResponseEntity<ProjectDTO> createProject(@RequestBody ProjectDTO projectDTO,
                                     @RequestHeader("X-User-Name") String username) {
 
         if (!StringUtils.hasText(projectDTO.getDescription())
         || !StringUtils.hasText(projectDTO.getName())) {
-            throw new RuntimeException("Invalid DTO");
+            return ResponseEntity.badRequest().build();
         }
 
-        return projectService.createProject(projectDTO, username);
+        ProjectDTO projectDto = projectService.createProject(projectDTO, username);
+        return new ResponseEntity<>(projectDto, HttpStatus.CREATED);
     }
 
     @PutMapping("/projects/{projectId}")
-    public ProjectDTO updateProject(@PathVariable int projectId, @RequestBody ProjectDTO projectDTO, @RequestHeader("X-User-Name")  String username) {
+    public ResponseEntity<ProjectDTO> updateProject(@PathVariable int projectId, @RequestBody ProjectDTO projectDTO, @RequestHeader("X-User-Name")  String username) {
         if (projectDTO.getId() == null) {
-            throw new RuntimeException("Bad request, id must not be null when updating a project");
+            log.error("Bad request, id must not be null when updating a project");
+            return ResponseEntity.badRequest().build();
         }
         if (projectDTO.getId() != projectId) {
-            throw new RuntimeException("Bad request, id in path and in body must be the same");
+            log.error("Bad request, id in path and in body must be the same");
+            return ResponseEntity.badRequest().build();
         }
-        return projectService.updateProject(projectDTO, username);
+        ProjectDTO updatedProject = projectService.updateProject(projectDTO, username);
+        return new ResponseEntity<>(updatedProject, HttpStatus.OK);
     }
 
     @PatchMapping("/projects/{projectId}")
-    public ProjectDTO patchProject(@PathVariable int projectId, @RequestBody ProjectDTO projectDTO, @RequestHeader("X-User-Name")  String username) {
+    public ResponseEntity<ProjectDTO> patchProject(@PathVariable int projectId, @RequestBody ProjectDTO projectDTO, @RequestHeader("X-User-Name")  String username) {
         if (projectDTO.getId() == null) {
-            throw new RuntimeException("Bad request, id must not be null when updating a project");
+            log.error("Bad request, id must not be null when updating a project");
+            return ResponseEntity.badRequest().build();
         }
         if (projectDTO.getId() != projectId) {
-            throw new RuntimeException("Bad request, id in path and in body must be the same");
+            log.error("Bad request, id in path and in body must be the same");
+            return ResponseEntity.badRequest().build();
         }
-        return projectService.patchProject(projectDTO, username);
+        return ResponseEntity.ok(projectService.patchProject(projectDTO, username));
     }
 
     @DeleteMapping("/projects/{projectId}")
-    public void deleteProject(@PathVariable Integer projectId, @RequestHeader("X-User-Name") String username) {
+    public ResponseEntity<Void> deleteProject(@PathVariable Integer projectId, @RequestHeader("X-User-Name") String username) {
         projectService.deleteProject(projectId, username);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
     }
 
 
