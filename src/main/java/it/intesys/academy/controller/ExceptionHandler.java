21de1fcf387f427617f0162cfa536bd86bf21301
Enrico Costanzi <enrico.costanzi@intesys.it>
2023-06-28 08:25:52 +0200

    lesson9 - handle runtime exception

--- src/main/java/it/intesys/academy/controller/ExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package it.intesys.academy.controller;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-@RestControllerAdvice
-public class ExceptionHandler extends ResponseEntityExceptionHandler {
-}
-
