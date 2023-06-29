cb0a51f332f5a6cf5269451db79d28ef37fc7112
MATTIA ZATTONI <mattizatto@gmail.com>
2023-06-29 17:46:54 +0200

    lesson9 - done

--- src/main/java/it/intesys/academy/repository/UserProjectRepository.java
+++ src/main/java/it/intesys/academy/repository/UserProjectRepository.java
@@ -2,10 +2,15 @@ package it.intesys.academy.repository;
 
 import it.intesys.academy.dto.UserProjectDTO;
 import it.intesys.academy.entity.User_Projects;
+import it.intesys.academy.service.IssueService;
 import jakarta.persistence.EntityManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 import java.util.List;
@@ -18,20 +23,32 @@ public class UserProjectRepository {
 
     private final EntityManager em;
 
-    public UserProjectRepository(NamedParameterJdbcTemplate jdbcTemplate, EntityManager em) {
+    private final RestTemplate restTemplate;
+    private static final Logger log = LoggerFactory.getLogger(IssueService.class);
+
+    public UserProjectRepository(NamedParameterJdbcTemplate jdbcTemplate, EntityManager em,RestTemplate restTemplate) {
 
         this.jdbcTemplate = jdbcTemplate;
         this.em = em;
+        this.restTemplate = restTemplate;
     }
 
     public Optional<User_Projects> usernameProjectVisibility(String username, Integer projectId) {
-
+        try{
+            ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:3003/api/v1/projects/"+username,String.class);
+            log.info("code"+response.getStatusCode());
+            log.info(response.getBody());
+            return Optional.empty();
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        /**
         User_Projects userProjects = em.createQuery("FROM User_Projects where projectId = (:projectId) and username = (:username)",User_Projects.class)
                 .setParameter("projectId", projectId)
                 .setParameter("username", username)
                 .getSingleResult();
 
-        return Optional.ofNullable(userProjects);
+        return Optional.ofNullable(userProjects);*/
     }
 
     public List<User_Projects> getUserProjects(String username) {
