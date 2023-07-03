7b4abdbc2b6638c39f1893fba29da41346045960
Enrico Costanzi <enrico.costanzi@intesys.it>
2023-07-03 15:37:39 +0200

    lesson12 - oauth2 resource server

--- src/main/java/it/intesys/academy/configuration/SecurityConfiguration.java
+++ src/main/java/it/intesys/academy/configuration/SecurityConfiguration.java
@@ -3,17 +3,17 @@ package it.intesys.academy.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true, securedEnabled = true)
@@ -27,11 +27,31 @@ public class SecurityConfiguration {
                         .requestMatchers("/api/**").hasAnyRole("USER", "ADMIN")
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults())
+                //.httpBasic(Customizer.withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(authConverter())
+                        )
+                )
                 .build();
     }
 
-    @Bean
+    private Converter<Jwt, AbstractAuthenticationToken> authConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            var roles = jwt.getClaimAsStringList("roles");
+            return roles
+                    .stream()
+                    .map(roleName -> new SimpleGrantedAuthority("ROLE_" + roleName))
+                    .collect(Collectors.toList());
+        });
+
+        return jwtAuthenticationConverter;
+
+    }
+
+
+    /*@Bean
     public UserDetailsService userDetailsService() {
         UserDetails enricoc =
                 User.withUsername("ecostanzi")
@@ -51,5 +71,5 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
+    }*/
 }
