e44ac98447d960190e5a55a554af75a1b5cdbf8b
zeno_begnini <zeno.begnini@gmail.com>
2023-07-04 12:50:39 +0200

    Authentication with token

--- src/main/java/it/intesys/academy/service/SecurityUtils.java
+++ src/main/java/it/intesys/academy/service/SecurityUtils.java
@@ -1,14 +1,19 @@
 package it.intesys.academy.service;
 
+import com.nimbusds.jwt.JWT;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 public class SecurityUtils {
 
-    public static String getCurrentUser() {
+/*    public static String getCurrentUser() {
         return SecurityContextHolder.getContext()
                 .getAuthentication()
                 .getName();
+    }*/
+    public static String getCurrentUser(){
+        return ((Jwt)SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getClaim("preferred_username");
     }
     public static boolean hasAuthority(String authority) {
         return SecurityContextHolder.getContext()
