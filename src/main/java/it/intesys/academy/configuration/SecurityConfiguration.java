c1b7c6e440d8052474877f5718575b0f80c99717
MATTIA ZATTONI <mattizatto@gmail.com>
2023-07-04 12:52:11 +0200

    lesson11 - oauth2

--- src/main/java/it/intesys/academy/configuration/SecurityConfiguration.java
+++ src/main/java/it/intesys/academy/configuration/SecurityConfiguration.java
@@ -2,17 +2,24 @@ package it.intesys.academy.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true, securedEnabled = true)
@@ -27,15 +34,34 @@ public class SecurityConfiguration {
                         .requestMatchers("/api/**").hasAnyRole("USER", "ADMIN")
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults()) //va a creare un filter che andra a controllare gli header http e inserira nel thread le info
+                //.httpBasic(Customizer.withDefaults()) //va a creare un filter che andra a controllare gli header http e inserira nel thread le info
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(authConverter())
+                        )
+                )
                 .build();
+    }
+
+    private Converter<Jwt, AbstractAuthenticationToken> authConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            var roles = jwt.getClaimAsStringList("roles");
+            return roles
+                    .stream()
+                    .map(roleName -> new SimpleGrantedAuthority("ROLE_" + roleName))
+                    .collect(Collectors.toList());
+        });
+
+        return jwtAuthenticationConverter;
+
     }
 
     /**
      *
      * @return UserDetails (username, password, authorities(ruoli))
      */
-    @Bean
+    /*@Bean
     public UserDetailsService userDetailsService() {
         UserDetails enricoc =
                 User.withUsername("ecostanzi")
@@ -56,15 +82,16 @@ public class SecurityConfiguration {
                         .build();
 
         return new InMemoryUserDetailsManager(enricoc, enricoo,mattiaz); //mini database con questi due user
-    }
+    }*/
 
     /**
      * Qualora inserisca utenti le password saranno bicript
      * @return
      */
-    @Bean
+    /*@Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    */
 }
 
