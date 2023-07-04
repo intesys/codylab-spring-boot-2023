a562686d5bddfbe054deaf998cd4237a99f42448
MATTIA ZATTONI <mattizatto@gmail.com>
2023-07-04 12:59:50 +0200

    lesson11 - username

--- src/main/java/it/intesys/academy/service/SecurityUtils.java
+++ src/main/java/it/intesys/academy/service/SecurityUtils.java
@@ -2,13 +2,14 @@ package it.intesys.academy.service;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 import java.util.Collection;
 import java.util.List;
 
 public class SecurityUtils {
     public static String getUserName(){
-        return SecurityContextHolder.getContext().getAuthentication().getName();
+        return ((Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getClaim("preferred_username");
     }
 
     public static List<String> getRoles(){
